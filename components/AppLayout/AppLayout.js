72d33fcb54831ab008de694f048b28b9711ff96d
sakibsarker <sakib15-3824@diu.edu.bd>
2024-01-23 00:04:53 +0600

    layout footer

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export const AppLayout = ({ children }) => {
+  const { user, error, isLoading } = useUser();
   return (
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
 
@@ -11,7 +15,28 @@ export const AppLayout = ({ children }) => {
           <div>tokens</div>
         </div>
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-300">list of posts</div>
-        <div className="bg-cyan-800">User information - logout button</div>
+        <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+        {!!user ? (
+          <>
+            <div className="min-w-[50px]">
+              <Image
+                src={user.picture}
+                alt={user.name}
+                height={50}
+                width={50}
+                className="rounded-full"
+              />
+            </div>
+            <div className="flex-1">
+            <p className="font-bold">{user.email}</p>
+            <Link className="text-sm" href="/api/auth/logout">Logout</Link>
+            </div>
+            
+          </>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
+        </div>
       </div>
 
       <div>{children}</div>
