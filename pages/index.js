bd3a9c0c3c9fe2f7e3e8cf33eb9d2ad07162f948
sakibsarker <sakib15-3824@diu.edu.bd>
2024-01-22 16:54:32 +0600

    auth

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  console.log(user);
+
+  console.log("USER:", user);
   return (
     <div>
       <h1>Home page</h1>
       <div>
-        <Link href='/api/auth/login'>Login</Link>
+        {!!user ? (
+          <>
+            <div>
+              <h2>Name: {user.name}</h2>
+              <p>Email: {user.email}</p>
+            </div>
+            <Link href="/api/auth/logout">Logout</Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login">Login</Link>
+        )}
       </div>
     </div>
-  )
+  );
 }
