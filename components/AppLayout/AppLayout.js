eb1b962d98beec1f9d294c6b06a417e9b2fa7865
sakibsarker <sakib15-3824@diu.edu.bd>
2024-01-23 17:25:40 +0600

    btn custom css

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -14,7 +14,7 @@ export const AppLayout = ({ children }) => {
       <div className="flex flex-col text-white overflow-hidden">
         <div className="bg-slate-800 px-2">
           <Logo/>
-          <Link className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block" href='/post/new'>New post</Link>
+          <Link className="btn" href='/post/new'>New post</Link>
           <Link className="block mt-2 text-center" href='/token-topup'>
             <FontAwesomeIcon icon={faCoins} className="text-yellow-500"/>
             <span className="pl-1"> 0 tokens available</span>
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="text text-justify">Revolutionize marketing with cutting-edge AI-generated content.
            Elevate your brand, captivate audiences, and stay ahead of the competition. 
            Transforming posts into powerful marketing assets with innovative artificial intelligence solutions.</p>
-        <Link className="btn" href='/post/new'>Start Now</Link>
+        <Link className="btn mt-2" href='/post/new'>Start Now</Link>
       </div>
     </div>
   );
