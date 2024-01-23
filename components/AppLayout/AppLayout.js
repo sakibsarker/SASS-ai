85a6eff3acc6380fb4b1f99be28da21c073a18b7
sakibsarker <sakib15-3824@diu.edu.bd>
2024-01-23 13:25:16 +0600

    coin icon

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -2,6 +2,8 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const AppLayout = ({ children }) => {
   const { user, error, isLoading } = useUser();
@@ -9,10 +11,13 @@ export const AppLayout = ({ children }) => {
     <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
 
       <div className="flex flex-col text-white overflow-hidden">
-        <div className="bg-slate-800">
+        <div className="bg-slate-800 px-2">
           <div>Logo</div>
-          <div>cta button</div>
-          <div>tokens</div>
+          <Link className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block" href='/post/new'>New post</Link>
+          <Link className="block mt-2 text-center" href='/token-topup'>
+            <FontAwesomeIcon icon={faCoins} className="text-yellow-500"/>
+            <span className="pl-1"> 0 tokens available</span>
+         </Link>
         </div>
         <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-300">list of posts</div>
         <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { AppLayout } from '@/components/AppLayout'
 
 export default function TokenTopup() {
   return (
     <div>TokenTopup</div>
   )
+}
+
+
+TokenTopup.getLayout=function getLayout(page,pageProps){
+  return<AppLayout {...pageProps}>{page}</AppLayout>
 }
