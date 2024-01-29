897f7294415a3363c72a2789307ed2ae4eb25be1
sakibsarker <sakib15-3824@diu.edu.bd>
2024-01-29 23:30:46 +0600

    generate api demo

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -2,7 +2,7 @@ import React from 'react'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default function Post(props) {
-  console.log('NEW PROPS PAGE',props)
+
   return (
     <div>Post page</div>
   )
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { AppLayout } from '@/components/AppLayout';
+import React from "react";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "@/components/AppLayout";
 
 export default function NewPost(props) {
-  console.log('NEW POST PROPS',props)
+
+  const handleClick = async () => {
+    const response=await fetch(`/api/generatepost`,{
+      method:"POST"
+    });
+    const json=await response.json();
+    console.log(json)
+
+  };
   return (
-    <div>New</div>
-  )
+    <div>
+      <h1>This is new post</h1>
+
+      <button className="btn" onClick={handleClick}>
+        Generate
+      </button>
+    </div>
+  );
 }
 
-NewPost.getLayout=function getLayout(page,pageProps){
-  return<AppLayout {...pageProps}>{page}</AppLayout>
-}
+NewPost.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
 
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps() {
     return {
       props: {
-        test: 'this is a test',
+        test: "this is a test",
       },
     };
   },
