1e13a4fa8966c451ab64306bfca793074b0ae046
sakibsarker <sakib15-3824@diu.edu.bd>
2024-03-12 14:10:28 +0600

    added bg color

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ import { Logo } from '../components/logo/index'
 export default function Home() {
 
   return (
-    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
-      <Image src={HeroImage} alt="Hero" fill className="absolute"/>
-      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/50 rounded backdrop-blur-sm">
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative bg-slate-800">
+     
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-500/50 rounded backdrop-blur-sm">
         <Logo/>
         <p className="text text-justify">Revolutionize marketing with cutting-edge AI-generated content.
            Elevate your brand, captivate audiences, and stay ahead of the competition. 
