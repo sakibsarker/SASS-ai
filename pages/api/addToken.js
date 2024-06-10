257780de3dc2cbb6e99dd80b3e33fcdd2d48caff
sakibsarker <sakib15-3824@diu.edu.bd>
2024-06-11 02:44:38 +0600

    added user api

--- pages/api/addToken.js
+++ pages/api/addToken.js
@@ -1,39 +1,38 @@
 import clientPromise from "@/lib/mongodb";
 import { getSession } from "@auth0/nextjs-auth0";
-import stripeInit from 'stripe';
+import stripeInit from "stripe";
 
-const stripe=stripeInit(process.env.STRIPE_SECRET_KEY);
-
-
+const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   const { user } = await getSession(req, res);
 
-  const lineItems=[{
-    price:process.env.STRIPE_PRODUCT_PRICE_ID,
-    quantity:1
-  }];
+  const lineItems = [
+    {
+      price: process.env.STRIPE_PRODUCT_PRICE_ID,
+      quantity: 1,
+    },
+  ];
 
-  const protocal=process.env.NODE_ENV==='development'?"http://":"https://";
+  const protocal =
+    process.env.NODE_ENV === "development" ? "http://" : "https://";
   const host = req.headers.host;
 
-
-  const checkoutSession=await stripe.checkout.sessions.create({
-    line_items:lineItems,
-    mode:"payment",
-    success_url:`${protocal}${host}/success`,
-    payment_intent_data:{
-      metadata:{
-        sub:user.sub,
-      }
+  const checkoutSession = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: `${protocal}${host}/success`,
+    payment_intent_data: {
+      metadata: {
+        sub: user.sub,
+      },
     },
-    metadata:{
-      sub:user.sub,
-    }
-  })
-
+    metadata: {
+      sub: user.sub,
+    },
+  });
 
   // console.log("user:", user);
 
-  res.status(200).json({ session:checkoutSession });
+  res.status(200).json({ session: checkoutSession });
 }
